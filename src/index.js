70e5cd0aed91c10a383b196da82d6e7b691b9ea4
natgmac <nathaniel.g.macdonald@gmail.com>
2025-02-03 00:15:56 -0500

    trying the hash thing

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
-// Set the basename to your repository name
 root.render(
-  <BrowserRouter basename="/yardsale">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
