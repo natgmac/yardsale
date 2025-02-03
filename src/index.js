8caa8fd534bca60c27700af80ab3d51f57b592ec
natgmac <nathaniel.g.macdonald@gmail.com>
2025-02-03 00:17:26 -0500

    going back to first try

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
+// Set the basename to your repository name
 root.render(
-  <HashRouter>
+  <BrowserRouter basename="/yardsale">
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
