19f375bd2d22cbf313a51d56e67b27648106465c
natgmac <nathaniel.g.macdonald@gmail.com>
2025-02-03 00:13:16 -0500

    fixed /yardsale main site

--- src/index.js
+++ src/index.js
@@ -2,18 +2,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
-import theme from './theme';
 import './index.css';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
+// Set the basename to your repository name
 root.render(
-  <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ThemeProvider>
+  <BrowserRouter basename="/yardsale">
+    <App />
+  </BrowserRouter>
 );
